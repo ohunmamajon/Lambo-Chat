5eae98420d5dfc49ae054c429c401d82e3977070
ohunmamajon <97847786+ohunmamajon@users.noreply.github.com>
2022-02-13 03:46:08 +0900

    Initial Commit

---
+++ Lambo Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lambo Chat
+//
+//  Created by Okhunjon Mamajonov on 2022/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
